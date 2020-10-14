962853f1f3118e6395d61767a3d15a33737e3932
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-14 11:56:43 +0700

    Add files via upload

---
+++ Keamanan.sh
@@ -0,0 +1,57 @@
+clear
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+sleep 1
+figlet KEAMANAN | lolcat
+figlet FACEBOOK | lolcat
+echo "===========================================" | lolcat
+echo "•          [ Authors : Sandi950 ]         •" | lolcat
+echo "•         [ YouTube  :  Sandi Yt ]        •" | lolcat
+echo "===========================================" | lolcat
+echo
+sleep 1
+echo -n "Nama Akun Facebook Anda : ";read nn
+sleep 3
+echo "Sedang Mengamankan Akun Anda" | lolcat
+sleep 3
+echo "Loading...." | lolcat
+sleep 4
+echo "10%" | lolcat
+sleep 4
+echo "20%" | lolcat
+sleep 4
+echo "30%" | lolcat
+sleep 4
+echo "40%" | lolcat
+sleep 4
+echo "50%" | lolcat
+sleep 4
+echo "60%" | lolcat
+sleep 4
+echo "70%" | lolcat
+sleep 4
+echo "80%" | lolcat
+sleep 4
+echo "90%" | lolcat
+sleep 4
+echo "100%" | lolcat
+sleep 5
+echo
+echo
+echo "1.Aman Dari Sesi ✓" | lolcat
+echo "2.Tidak Bisa Di Hack ✓" | lolcat
+echo "3.Tidak Bisa Di Report ✓" | lolcat
+echo "4.Tidak Bisa Di Kenang ✓" | lolcat
+echo "5.Semuanya Selesai ✓" | lolcat
+sleep 1
+figlet SUKSES | lolcat
